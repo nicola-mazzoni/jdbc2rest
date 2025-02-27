3d2a70c9c8450caeff81dc60c5434a4cd97ea566
Riccardo <riccardo.neuralhr.it>
2025-02-27 09:51:14 +0100

    add dynamic throttle

--- src/main/java/jdbc2rest/entities/configuration/Server.java
+++ src/main/java/jdbc2rest/entities/configuration/Server.java
@@ -1,19 +1,34 @@
 package jdbc2rest.entities.configuration;
 
 public class Server {
+
+	private int port;
+
+	private Ssl ssl;
+
+	private int throttle;
+
 	public int getPort() {
 		return port;
 	}
+
 	public void setPort(int port) {
 		this.port = port;
 	}
+
 	public Ssl getSsl() {
 		return ssl;
 	}
+
 	public void setSsl(Ssl ssl) {
 		this.ssl = ssl;
 	}
-	private int port;
-	
-	private Ssl ssl;
+
+	public int getThrottle() {
+		return throttle;
+	}
+
+	public void setThrottle(int throttle) {
+		this.throttle = throttle;
+	}
 }
--- src/main/java/jdbc2rest/services/Jdbc2RestRouteBuilder.java
+++ src/main/java/jdbc2rest/services/Jdbc2RestRouteBuilder.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import jdbc2rest.MainProcessing;
 import jdbc2rest.entities.Request;
 import jdbc2rest.entities.Response;
+import jdbc2rest.entities.configuration.Server;
 
 public class Jdbc2RestRouteBuilder extends RouteBuilder {
 
@@ -20,22 +21,22 @@ public class Jdbc2RestRouteBuilder extends RouteBuilder {
 
 		final Logger logger = LoggerFactory.getLogger(Jdbc2RestRouteBuilder.class);
 
-		String port = new String(MainProcessing.getJdbc2RestConfiguration().getServer().getPort() + "");
+		Server server = new Server();
+		server = MainProcessing.getJdbc2RestConfiguration().getServer();
 
-		if (MainProcessing.getJdbc2RestConfiguration().getServer().getSsl() != null) {
+		if (server.getSsl() != null) {
 
 			logger.info("SSL configuration...");
 
 			try {
 				KeyStoreParameters ksp = new KeyStoreParameters();
-				ksp.setResource(MainProcessing.getJdbc2RestConfiguration().getServer().getSsl().getKeystore().getJks());
+				ksp.setResource(server.getSsl().getKeystore().getJks());
 				ksp.setPassword(
-						MainProcessing.getJdbc2RestConfiguration().getServer().getSsl().getKeystore().getPassword());
+						server.getSsl().getKeystore().getPassword());
 
 				KeyManagersParameters kmp = new KeyManagersParameters();
 				kmp.setKeyStore(ksp);
-				kmp.setKeyPassword(
-						MainProcessing.getJdbc2RestConfiguration().getServer().getSsl().getKeystore().getPassword());
+				kmp.setKeyPassword(server.getSsl().getKeystore().getPassword());
 
 				SSLContextParameters scp = new SSLContextParameters();
 				scp.setKeyManagers(kmp);
@@ -43,7 +44,9 @@ public class Jdbc2RestRouteBuilder extends RouteBuilder {
 				JettyHttpComponent j = getContext().getComponent("jetty", JettyHttpComponent.class);
 				j.setSslContextParameters(scp);
 
-				restConfiguration().component("jetty").host("0.0.0.0").port(port).scheme("https")
+				restConfiguration()
+						.component("jetty").host("0.0.0.0")
+						.port(server.getPort()).scheme("https")
 						.bindingMode(RestBindingMode.off);
 
 				logger.info("SSL ACTIVE");
@@ -51,18 +54,18 @@ public class Jdbc2RestRouteBuilder extends RouteBuilder {
 			} catch (Exception e) {
 				logger.error("Error on SSL configuration");
 				e.printStackTrace();
-				// TODO: handle exception
 			}
 
 		} else {
 
-			restConfiguration().component("jetty").host("0.0.0.0").port(port).scheme("http")
+			restConfiguration().component("jetty")
+					.host("0.0.0.0").port(server.getPort()).scheme("http")
 					.bindingMode(RestBindingMode.json);
-			logger.warn("SSL NOT ACTIVE, DATA TRANSFERT NOT SECURE");
 
+			logger.warn("SSL NOT ACTIVE, DATA TRANSFERT NOT SECURE");
 		}
 
-		logger.info("port:" + MainProcessing.getJdbc2RestConfiguration().getServer().getPort());
+		logger.info("port:" + server.getPort());
 		logger.info("Server running...");
 
 		/*
@@ -70,12 +73,13 @@ public class Jdbc2RestRouteBuilder extends RouteBuilder {
 		 * .to("file:/tmp/?fileName=HealthCheck_${date:now:yyyy_MM_dd_hhmmss}.json");
 		 */
 
-		rest("/jdbc2rest").type(Request.class).outType(Response.class)
+		rest("/jdbc2rest")
 				.post("/v1")
+				.type(Request.class).outType(Response.class)
 				.to("direct:jdbc2rest");
 
 		from("direct:jdbc2rest")
-				.throttle(100)
+				.throttle(server.getThrottle())
 				.to("bean:jdbc2rest.services.SqlExecutor");
 
 	}
